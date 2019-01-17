cca5c8603815053828e1f2178dfa21fc20d0fa94
reboot-ua <reboot.if.ua@gmail.com>
2019-01-17 21:52:39 +0200

    click

---
+++ click.js
@@ -0,0 +1,12 @@
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+
+    document.body.style.background = bgColor;
+}
+
+
+document.getElementById('change').onclick = random_bg_color;
