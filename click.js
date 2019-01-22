ebe1910ca9456aa7896d719ae6fe89e53068595b
reboot-ua <reboot.if.ua@gmail.com>
2019-01-22 21:26:59 +0200

    fix

--- click.js
+++ click.js
@@ -1,11 +1,14 @@
 let NewArr = [];
 let i = 0;
-
 function prevItem() {
-    if (i === 0) {
-        i = NewArr.length}
+    if (i === 0)
+    {
+        i = NewArr.length
+    }
     else
-    {  NewArr.pop();}
+    {
+        NewArr.pop();
+    }
     i = i - 1;
     return NewArr[i];
 }
@@ -15,11 +18,15 @@ function random_bg_color() {
     let z = Math.floor(Math.random() * 256);
     let bgColor = "rgb(" + x + "," + y + "," + z + ")";
     console.log(bgColor);
-    document.getElementsByClassName('page').style.background = bgColor;
+    document.body.style.background = bgColor;
     NewArr.push(bgColor);
     console.log(NewArr);
     i = i + 1;
     return NewArr[i];
 }
-        document.getElementById('change').onclick = random_bg_color;
-        document.getElementById('back').onclick = document.getElementById('page').style.background = prevItem();
+    document.getElementById('change').onclick = random_bg_color;
+    document.getElementById('back').onclick =
+        function() {
+            document.getElementById('page').style.background = prevItem();
+        };
+console.log(prevItem());
