b627c98d232ab8dfefc2f46c92891310a0735ad4
reboot-ua <reboot.if.ua@gmail.com>
2019-01-22 19:03:45 +0200

    button back

--- click.js
+++ click.js
@@ -1,45 +1,25 @@
 let NewArr = [];
-var i = 0;
-
-function nextItem() {
-    i = i + 1;
-    i = i % NewArr.length;
-    return NewArr[i];
-}
+let i = 0;
 
 function prevItem() {
     if (i === 0) {
         i = NewArr.length}
     else
     {  NewArr.pop();}
-
     i = i - 1;
     return NewArr[i];
 }
-
 function random_bg_color() {
     let x = Math.floor(Math.random() * 256);
     let y = Math.floor(Math.random() * 256);
     let z = Math.floor(Math.random() * 256);
     let bgColor = "rgb(" + x + "," + y + "," + z + ")";
     console.log(bgColor);
-
-    document.body.style.background = bgColor;
-
+    document.getElementsByClassName('page').style.background = bgColor;
     NewArr.push(bgColor);
     console.log(NewArr);
-
+    i = i + 1;
+    return NewArr[i];
 }
-
-
-
-document.getElementById('change').onclick = random_bg_color , nextItem;
-
-document.getElementById('back').addEventListener(
-    'click',
-    function () {
-        document.getElementById('page').style.background = prevItem();
-    }
-);
-
-
+        document.getElementById('change').onclick = random_bg_color;
+        document.getElementById('back').onclick = document.getElementById('page').style.background = prevItem();
