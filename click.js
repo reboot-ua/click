d6d448c06f422a55910d1d420e7aa2eb8ca34ac8
reboot-ua <reboot.if.ua@gmail.com>
2019-03-04 20:04:03 +0200

    pagination_2

--- click.js
+++ click.js
@@ -1,5 +1,29 @@
 let NewArr = [];
 let i = 0;
+function prevItem() {
+    if (i <= 0) {
+        i = NewArr.length;
+        document.getElementById('back').disabled = true;
+        }
+    i = i - 1;
+    document.getElementById('next').disabled = false;
+    return NewArr[i];
+}
+function nextItem () {
+        i = i + 1;
+    i = i % NewArr.length ;
+    return NewArr[i];
+}
+function pagination (value, index){
+    const pageButton = document.createElement('span');
+    pageButton.textContent = index+1 + ' ';
+    pageButton.onclick = function (){
+        document.getElementById('page').style.background = NewArr[index];
+        document.getElementById('back').disabled = false;
+        document.getElementById('next').disabled = false;
+    };
+    document.getElementById('paginator').appendChild(pageButton)
+}
 function random_bg_color() {
     let x = Math.floor(Math.random() * 256);
     let y = Math.floor(Math.random() * 256);
@@ -17,59 +41,25 @@ console.log(NewArr);
         if (NewArr.indexOf( bgColor=== -1)) {
             document.body.style.background = bgColor;
             NewArr.push(bgColor);
-        }else {
+        } else {
             NewArr.splice(i, NewArr.length);
             NewArr.push(bgColor);
         }
     }
-    function pagination (value, index){
-        const pageButton = document.createElement('span');
-        pageButton.textContent = index+1 + ' ';
-        pageButton.onclick = function (){
-            document.getElementById('page').style.background = NewArr[index];
-            document.getElementById('back').disabled = false;
-            document.getElementById('next').disabled = false;
-
-        };
-        document.getElementById('paginator').appendChild(pageButton)
-    }
-    document.getElementById('paginator').textContent = '';
-        NewArr.forEach(pagination);
-
-    return false;
+       document.getElementById('paginator').textContent = '';
+         NewArr.forEach(pagination);
+     return false;
 }
 function add_new_color() {
     if (!NewArr.includes(document.getElementById('box').value)) {
         NewArr.push(document.getElementById('box').value);
         document.body.style.background = document.getElementById('box').value;
         document.getElementById('back').disabled = false;
+            document.getElementById('paginator').textContent = '';
+            NewArr.forEach(pagination);
+    console.log(NewArr);
     } return false;
 }
-function prevItem() {
-    if (i <= 0) {
-        i = NewArr.length
-        }
-    i = i - 1;
-    document.getElementById('next').disabled = false;
-    if (i <= 0) {
-        document.getElementById('back').disabled = true;
-
-    }
-    return NewArr[i];
-}
-function nextItem () {
-    i = i + 1;
-    i = i % NewArr.length ;
-    return NewArr[i];
-    if(i=NewArr.length-1) {
-        document.getElementById('next').disabled = true;
-
-    }
-}
-
-
-
-
     document.getElementById('change').onclick = random_bg_color;
     document.getElementById('back').onclick =
         function() {
@@ -79,7 +69,7 @@ function nextItem () {
         function() {
             document.getElementById('page').style.background = nextItem();
         };
-//paginator
+
 
 
 
